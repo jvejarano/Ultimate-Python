968430f9533d33991a8195f69e82722664d9bab0
Jorge Vejarano Moreno <jorgevejarano@gmail.com>
2024-04-09 11:52:14 -0400

    Args, kwargs, return, alcance.

--- funciones/02-xargs.py
+++ funciones/02-xargs.py
@@ -2,4 +2,10 @@ def suma(*numeros):
     resultado = 0
     for numero in numeros:
         resultado += numero
-        print(resultado)
+    # OJO la identacion de este "print" si hay un tab + sumaria mal, OJO
+    print(resultado)
+
+
+suma(2, 5, 7)
+suma(2, 5)
+suma(2, 8, 7, 45, 32)
