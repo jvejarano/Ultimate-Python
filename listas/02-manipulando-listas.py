4831e6c66611405945452a75f8272f746807e382
Jorge Vejarano Moreno <jorgevejarano@gmail.com>
2024-04-26 11:03:08 -0400

    Se continua aprendiendo manipulacion de listas y tuplas

--- listas/02-manipulando-listas.py
+++ listas/02-manipulando-listas.py
@@ -1,2 +1,10 @@
 mascotas = ["Tango", "Jack", "Chapita", "Max"]
+print(mascotas[0])
+mascotas[0] = "Bicho"
 print(mascotas)
+# print(mascotas[2:])
+# print(mascotas[-1])
+# print(mascotas[1:2:2])
+numeros = list(range(21))
+print(numeros[::2])
+print(numeros[1::2])
