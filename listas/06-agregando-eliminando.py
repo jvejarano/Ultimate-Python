0e0a9bc3c87eef68affcf87076da291f8e7ef1cd
Jorge Vejarano Moreno <jorgevejarano@gmail.com>
2024-05-14 10:57:36 -0400

    Continuando el aprendizaje de manejo de listas en Python

---
+++ listas/06-agregando-eliminando.py
@@ -0,0 +1,15 @@
+mascotas = [
+    "Wolfgang",
+    "Pelusa",
+    "Pulga",
+    "Felipe",
+    "Pulga",
+    "Chanchito Feliz",
+]
+mascotas.insert(1, "Melvin")  # Método INSERT
+mascotas.append("Chanchito triste")  # Método Append
+mascotas.remove("Pulga")  # Método Remove
+mascotas.pop()  # Método pop
+del mascotas[0]  # palabra reservada del
+mascotas.clear()  # Método clear
+print(mascotas)
