4171098ef82d315b621846c1506916ea586c6041
Jorge Vejarano Moreno <jorgevejarano@gmail.com>
2024-05-15 13:45:02 -0400

    Se agrega archivos del libro sobre Estructura de datos y algoritmos en Python

---
+++ Algoritmos-resueltos-Python.py
@@ -0,0 +1,11 @@
+print("------------------------------------------")
+print("Ejercicio1: CALCULAR DISTANCIA.")
+print("------------------------------------------")
+
+# Entradas
+print("Ingrese la velocidad y el tiempo de la unidad mòvil")
+V = float(input("Velocidad: "))
+T = int(input("Tiempo: "))
+D = V*T
+
+print(D)
